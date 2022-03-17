eff9c564df62176d3add5c400148eee0b79b16d2
zhx <xfortdev@gmail.com>
2022-03-17 13:36:26 +0800

    修复下载apk成zip文件的问题'

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"log"
+	"mime"
 	"net/http"
 	"os"
-	"log"
+	"path/filepath"
+	"strings"
 )
 
 func main() {
@@ -11,6 +14,8 @@ func main() {
 		log.Println("终止运行，启动参数必须包含ip:port,以及文件路径")
 		return
 	}
+	mime.AddExtensionType(".apk", "application/vnd.android.package-archive")
+
 	err := startFileServer(os.Args[1])
 	if err != nil {
 		log.Println(err)
@@ -20,7 +25,23 @@ func main() {
 func startFileServer(addr string) error {
 	serverFilePath := os.Args[2]
 	log.Println(serverFilePath)
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir(serverFilePath))))
+
+	http.HandleFunc("/static/", func(w http.ResponseWriter, r *http.Request) {
+		name := strings.Replace(r.URL.Path, "/static", serverFilePath, 1)
+		_, file := filepath.Split(name)
+		log.Println("path", file)
+		if file != "" {
+			cd := mime.FormatMediaType("attachment", map[string]string{"filename": file})
+			w.Header().Set("Content-Disposition", cd)
+			if strings.HasSuffix(file, "apk") {
+				w.Header().Set("Cache-Control", "no-cache")
+
+				w.Header().Set("Content-Type", "application/vnd.android.package-archive")
+			}
+		}
+		http.ServeFile(w, r, name)
+	})
+
 	err := http.ListenAndServe(addr, nil)
 	return err
 }
